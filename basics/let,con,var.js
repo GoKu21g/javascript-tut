8592122b5ba313e99703a2befcdf87e6dec3760c
Gourav <gouravkumar21g@gmail.com>
2025-06-28 17:14:32 +0530

    second

---
+++ basics/let,con,var.js
@@ -0,0 +1,29 @@
+const accountId = 144553
+let accountEmail = "gourav@gmail.com"
+var accountPassword = "ggaa220055"
+
+let accountState
+
+accountCity = "Jaipur"
+
+// accountId = 2 Not Allowed
+
+accountEmail = "dfcc@123.com"
+accountPassword = "2121212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// var can't use scope, thats why we use let
+
+/*
+    Mostly use let
+
+    if the value in not defined it is undefined
+
+*/
+
+
