c26bfbcf003b4fd0af43e64b8427a3797e782452
Gourav <gouravkumar21g@gmail.com>
2025-06-28 17:29:13 +0530

    third

---
+++ basics/datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3+3);
+
+// maintain code readability 
+
+//number => 2 to power 53
+//bigint 
+//string=>""
+//boolean=> true/false
+//null=>standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+let a = 33
+
+console.log(typeof a);
+
+console.log(typeof null);// null is oject while undefined in undefined
+
