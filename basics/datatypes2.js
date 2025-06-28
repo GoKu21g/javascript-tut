64cd33c40e03b8e135842ea2d553838e7facd862
Gourav <gouravkumar21g@gmail.com>
2025-06-28 17:41:17 +0530

    conversion

---
+++ basics/datatypes2.js
@@ -0,0 +1,50 @@
+let score = "33"
+
+console.log(typeof score);
+
+
+let valueinnumber = Number(score)
+
+console.log(typeof valueinnumber);
+
+let score2 = "33abc"
+
+console.log(typeof score2);
+
+
+let valueinnumber2 = Number(score2)
+
+console.log(typeof valueinnumber2);
+
+console.log(valueinnumber2);
+
+
+let score3 = null
+
+console.log(typeof score3);
+
+
+let valueinnumber3 = Number(score3)
+
+console.log(typeof valueinnumber3);
+
+console.log(valueinnumber3);
+
+let score4 = undefined
+
+console.log(typeof score4);
+
+
+let valueinnumber4 = Number(score4)
+
+console.log(typeof valueinnumber4);
+
+console.log(valueinnumber4);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true=>1; false=>0
+
+//""=>false
+//"Goku"=>true
