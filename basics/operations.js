ec683adabc11b1a99263b88f5bd61ff3444b3411
Gourav <gouravkumar21g@gmail.com>
2025-06-29 20:54:06 +0530

    comparision

---
+++ basics/operations.js
@@ -0,0 +1,21 @@
+
+
+// let value = 3
+// let negvalue = -value
+
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = "goku"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+
+let a = "1" + 2 - 3
+console.log(a);
+
+console.log(typeof a);
+
+
